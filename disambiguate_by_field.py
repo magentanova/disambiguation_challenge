7213f12308c29dfec60616815e72c67e14b2d539
Justin Richards <richards.justind@gmail.com>
2017-08-30 15:02:25 -0400

    created sparse names table

--- disambiguate_by_field.py
+++ disambiguate_by_field.py
@@ -7,19 +7,24 @@ assets = pandas.read_json('asset-test-sample-small.json')
 
 def generateSparseNamesDict():
 	namesDict = defaultdict(list)
-	for i,row in assets.iterrows():
-		for name in row['names']: 
-			if type(name['email']) == unicode:
-				pubsDict[name['email'].lower()].append(row['publication_id'])
-	return namesDIct
+	for i,row in identities.iterrows():
+		first,last = row['name'].split(' ')[0], row['name'].split(' ')[1]
+		namesDict[first[0].lower() + '_' + last.lower()].append(row)
+	return namesDict
 
-emailToPubs = generateEmailToPubsDict()
 
-idSets = []
-for i,row in identities.iterrows(): 
-	emailVal = row['email']
-	idSets.append(emailToPubs[emailVal])
-	# if type(emailVal) == unicode: 
+sparseNamesDict = generateSparseNamesDict()
 
-identities['publication_ids'] = idSets
-identities.reset_index().to_json('enriched_identities.json', orient='records')
+ct = 0
+for key in sparseNamesDict: 
+	if (len(sparseNamesDict[key]) > 1):
+		print key, len(sparseNamesDict[key])
+		ct += 1 
+
+print ct
+# for i,row in assets.iterrows(): 
+# 	for nameData in row['names']:
+
+
+# identities['publication_ids'] = idSets
+# identities.reset_index().to_json('identities_enriched_by_field.json', orient='records')
